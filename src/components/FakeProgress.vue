d5ca5210b06272320b4ab93211333349f83e390e
Gugle <administration@ztxy666.cn>
2025-06-30 20:59:33 +0800

    调整进度条流畅度

--- src/components/FakeProgress.vue
+++ src/components/FakeProgress.vue
@@ -14,14 +14,13 @@ function mounted() {
     } else {
       process.value = 0
     }
-  }, 100)
+  }, 1)
 }
 
 onMounted(mounted)
 
 function getProcess() {
-  let pro = 0.99 * (1 - Math.exp(-0.1 * process.value / 60))
-  pro = Number.parseFloat(pro.toFixed(2))
+  let pro = 0.99 * (1 - Math.exp(-0.1 * process.value / 100 / 60))
   pro = Math.min(pro, 0.99)
   if (!processing.value) {
     return 1.0
@@ -32,14 +31,26 @@ function getProcess() {
 
 <template>
   <a-progress
+      class="progress"
       :percent="getProcess()"
       :color="{
         '0%': 'rgb(var(--primary-6))',
         '100%': 'rgb(var(--success-6))',
       }"
+      :show-text="false"
   />
+  <div class="percent">
+    {{ (getProcess() * 100).toFixed(2) }}%
+  </div>
 </template>
 
 <style scoped>
-
+.progress {
+  width: calc(100% - 3.5rem);
+}
+.percent {
+  display: inline-flex;
+  width: 2.5rem;
+  margin-left: 1rem;
+}
 </style>
