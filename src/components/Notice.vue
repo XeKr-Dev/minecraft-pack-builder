be81606d66868d32cac5565a3ea290cdd3052ed4
Gugle <administration@ztxy666.cn>
2025-07-01 04:49:00 +0800

    将公告组件单独提出来

---
+++ src/components/Notice.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="notice-content">
+    <a-card>
+      <template #title>
+        公告
+      </template>
+      本页面是一个 <code>纯静态页面</code> ，部署于 <code>GitHub Pages</code>
+      <a-divider/>
+      本系统基于 <code>GitHub API</code> 实现，由于未登录用户访问限制每小时仅能发起 <code>60</code> 个 请求，请先在
+      <a-link href="https://github.com/settings/tokens/new" target="_blank"><code>GitHub</code></a-link>
+      上创建一个 <code>Personal Access Token</code> ，并于右上角登录，
+      需勾选的权限为： <code>public_repo</code> 与 <code>read:project</code>
+      <a-divider/>
+      本系统没有后端，<code>Personal Access Token</code> 在本地存储， 具体可通过 <code>Ctrl + Shift + I</code>
+      打开开发者工具查看 <code>网络通信</code> ，我方承诺不会发起除 <code>Github API</code> 之外的网络请求。
+      <a-divider/>
+      使用 <code>https://build.xekr.dev/#/USER/REPO</code> 分享构建页面，可以携带仓库信息，方便链接获得者快速构建
+    </a-card>
+    <a-card style="margin-top: 20px">
+      <template #title>
+        特别鸣谢 <a-link href="https://www.anvilcraft.dev/#/support" target="_blank"><icon-gift/></a-link>
+      </template>
+      虚位以待...
+    </a-card>
+  </div>
+</template>
+
+<style scoped>
+.notice-content {
+  min-width: 300px;
+  width: calc(100vw - 1880px);
+  margin: 20px;
+  display: inline-block;
+  line-height: 2;
+}
+
+.notice-content code {
+  background-color: var(--color-bg-5);
+  padding: 0.2em 0.4em;
+  border-radius: 4px;
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import {onMounted, type Ref, ref} from "vue";
-import {IconSunFill, IconMoonFill} from "@arco-design/web-vue/es/icon";
 import {Message} from "@/scripts/message";
 import MarkdownView from "@/components/MarkdownView.vue";
 import {GithubAPI} from "@/scripts/github";
@@ -10,6 +9,7 @@ import {Builder} from "@/scripts/builder";
 import {saveAs} from 'file-saver';
 import mc_version from '@/minecraft_version.json'
 import FakeProgress from "@/components/FakeProgress.vue";
+import Notice from "@/components/Notice.vue";
 
 const BASE_64_PNG_PREFIX = 'data:image/png;base64, '
 const minecraft_version: {
@@ -310,8 +310,8 @@ function build() {
     </template>
     <template #extra>
       <a-button class="btn" shape="circle" @click="click">
-        <IconMoonFill v-if="dark"/>
-        <IconSunFill v-else/>
+        <icon-moon-fill v-if="dark"/>
+        <icon-sun-fill v-else/>
       </a-button>
       <a-dropdown>
         <a-button class="btn">登录</a-button>
@@ -331,24 +331,7 @@ function build() {
     </a-form>
   </div>
   <div class="container">
-    <div class="notice-content">
-      <a-card>
-        <template #title>
-          公告
-        </template>
-        本页面是一个 <code>纯静态页面</code> ，部署于 <code>GitHub Pages</code>
-        <a-divider/>
-        本系统基于 <code>GitHub API</code> 实现，由于未登录用户访问限制每小时仅能发起 <code>60</code> 个 请求，请先在
-        <a-link href="https://github.com/settings/tokens/new" target="_blank"><code>GitHub</code></a-link>
-        上创建一个 <code>Personal Access Token</code> ，并于右上角登录，
-        需勾选的权限为： <code>public_repo</code> 与 <code>read:project</code>
-        <a-divider/>
-        本系统没有后端，<code>Personal Access Token</code> 在本地存储， 具体可通过 <code>Ctrl + Shift + I</code>
-        打开开发者工具查看 <code>网络通信</code> ，我方承诺不会发起除 <code>Github API</code> 之外的网络请求。
-        <a-divider/>
-        使用 <code>https://build.xekr.dev/#/USER/REPO</code> 分享构建页面，可以携带仓库信息，方便链接获得者快速构建
-      </a-card>
-    </div>
+    <notice />
     <div class="page-content">
       <a-card>
         <template v-if="repo !== ''" #title>
@@ -437,20 +420,6 @@ function build() {
 
 .container {
   display: flex;
-}
-
-.notice-content {
-  min-width: 300px;
-  width: calc(100vw - 1880px);
-  margin: 20px;
-  display: inline-block;
-  line-height: 2;
-}
-
-.notice-content code {
-  background-color: var(--color-bg-5);
-  padding: 0.2em 0.4em;
-  border-radius: 4px;
 }
 
 .page-content {
