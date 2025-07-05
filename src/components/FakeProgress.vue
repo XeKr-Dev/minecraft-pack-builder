84f1fd7c5a2b7ce1771664115200726003b1e6d0
Gugle <administration@ztxy666.cn>
2025-07-06 00:25:59 +0800

    调整进度条效果

--- src/components/FakeProgress.vue
+++ src/components/FakeProgress.vue
@@ -7,13 +7,17 @@ const processing = defineModel({
 
 let process = 0
 const percent = ref(0)
+let lastTime = Date.now()
 
 function mounted() {
   setInterval(() => {
     if (processing.value) {
-      process += 1
+      const nowTime = Date.now()
+      process += nowTime - lastTime
+      lastTime = nowTime
     } else {
       process = 0
+      lastTime = Date.now()
     }
     percent.value = getProcess(process)
   }, 1)
@@ -22,33 +26,69 @@ function mounted() {
 onMounted(mounted)
 
 function getProcess(process: number) {
-  let pro = 0.99 * (1 - Math.exp(-0.1 * process / 100 / 60))
-  pro = Math.min(pro, 0.9999)
   if (!processing.value) {
     return 1.0
   }
+  let pro = 0;
+  // 1500 20% 15000 50% 30000 99.99%
+  if (process < 15000) {
+    pro = process / 15000 * 0.2;
+  } else if (process < 150000) {
+    pro = (process - 15000) / 150000 * 0.3 + 0.2;
+  } else {
+    pro = (process - 15000) / 300000 * 0.4 + 0.5;
+  }
+  pro = Math.min(pro, 0.9999)
   return pro;
 }
 </script>
 
 <template>
-  <a-progress
-      class="progress"
-      :percent="percent"
-      :color="{
-        '0%': 'rgb(var(--primary-6))',
-        '100%': 'rgb(var(--success-6))',
-      }"
-      :show-text="false"
-  />
+  <div class="fake-progress">
+    <div :class="`fake-progress-inner fake-progress-${processing ? 'processing' : 'success'}`"/>
+  </div>
   <div class="percent">
     {{ (percent * 100).toFixed(2) }}%
   </div>
 </template>
 
 <style scoped>
-.progress {
+
+@keyframes progress {
+  0% {
+    width: 0;
+  }
+  5% {
+    width: 20%;
+  }
+  50% {
+    width: 50%;
+  }
+  100% {
+    width: 99%;
+  }
+}
+
+.fake-progress {
+  display: inline-flex;
   width: calc(100% - 3.5rem);
+  height: 5px;
+  border-radius: 2px;
+  background: rgb(var(--gray-3));
+}
+
+.fake-progress-inner {
+  height: 5px;
+  background: linear-gradient(to right, rgb(var(--primary-6)), rgb(var(--success-6)));
+  border-radius: 2px;
+}
+
+.fake-progress-processing {
+  animation: progress 300s;
+}
+
+.fake-progress-success {
+  width: 100%;
 }
 
 .percent {
