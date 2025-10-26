417a240ed0d9b2891c88185bf026e4845b98c5ed
Gugle <administration@ztxy666.cn>
2025-10-26 21:08:44 +0800

    feat(minecraft): 更新版本数据并完善类型定义
    - 新增多个 Minecraft 版本的 datapack 和资源版本信息
    - 补全版本类型字段（release/snapshot）
    - 完善 minecraft_versions.ts 中的数据结构定义
    - 修正部分版本的 datapack_version 和 resources_version 数值
    - 标记早期版本如 1.13、1.14 等为 release 类型
    - 清理注释代码，优化文件结构可读性

--- src/minecraft_versions.ts
+++ src/minecraft_versions.ts
@@ -1,27 +1,62 @@
-// import mc_version from './minecraft_version.json'
-// import * as fs from 'node:fs'
+// import axios from "axios";
+// import versionChangedFile from "./version_changed.json";
+// import * as fs from "node:fs";
 //
-// const mv: {
-//     [key: string]: {
-//         datapack_version: number,
-//         resources_version: number
-//     }
-// } = mc_version
-//
-// const mcv: {
-//     [key: string]: {
-//         type: "snapshot" | "release",
-//         datapack_version: number,
-//         resources_version: number
-//     }
-// } = {}
-// for (let mcVersionKey in mv) {
-//     const value = mv[mcVersionKey]
-//     mcv[mcVersionKey] = {
-//         type: /^\d+.\d+.\d+$/.test(mcVersionKey) ? "release" : "snapshot",
-//         datapack_version: value.datapack_version,
-//         resources_version: value.resources_version
-//     }
-//     console.log(JSON.stringify(mcv))
-//     fs.writeFileSync("minecraft_versions_1.json", JSON.stringify(mcv, null, 2))
+// interface MinecraftVersionData {
+//     id: string,
+//     type: "snapshot" | "release",
 // }
+//
+// interface MinecraftVersion {
+//     type: "snapshot" | "release",
+//     datapack_version: number,
+//     resources_version: number
+// }
+//
+// interface VersionChangedData {
+//     datapack_version?: number,
+//     resources_version?: number
+// }
+//
+// const versionChanged: { [key: string]: VersionChangedData } = versionChangedFile;
+// let minecraftVersions: { [key: string]: MinecraftVersion } = {};
+//
+// const INSTANCE = axios.create({
+//     baseURL: 'https://launchermeta.mojang.com',
+//     timeout: 15000
+// })
+//
+// const currentVersion = {
+//     datapack_version: 1,
+//     resources_version: 4
+// }
+//
+// INSTANCE.get('/mc/game/version_manifest_v2.json').then(res => {
+//     const versions = res.data.versions as MinecraftVersionData[];
+//     let savedVersions: MinecraftVersionData[] = [];
+//     for (const obj of versions) {
+//         savedVersions = [obj, ...savedVersions];
+//         if (obj.id == "17w43a") break;
+//     }
+//     for (const obj of savedVersions) {
+//         if (obj.id in versionChanged) {
+//             currentVersion.datapack_version = versionChanged[obj.id].datapack_version || currentVersion.datapack_version;
+//             currentVersion.resources_version = versionChanged[obj.id].resources_version || currentVersion.resources_version;
+//         }
+//         minecraftVersions[obj.id] = {
+//             type: obj.type,
+//             datapack_version: currentVersion.datapack_version,
+//             resources_version: currentVersion.resources_version
+//         }
+//     }
+//     let savedMinecraftVersions: { [key: string]: MinecraftVersion } = {}
+//     let savedMinecraftVersionKeys: string[] = []
+//     for (let key of Object.keys(minecraftVersions)) {
+//         savedMinecraftVersionKeys = [key, ...savedMinecraftVersionKeys]
+//     }
+//     for (let key of savedMinecraftVersionKeys) {
+//         savedMinecraftVersions[key] = minecraftVersions[key]
+//     }
+//     minecraftVersions = savedMinecraftVersions
+//     fs.writeFileSync("minecraft_version.json", JSON.stringify(minecraftVersions, null, 2))
+// })
